2a6ec286c7cc6d365528a0f3819827681c857fdd
CleoHammad <102500335+CleoHammad@users.noreply.github.com>
2022-04-11 15:05:53 +0400

    April 11

---
+++ A_Django_Project/a_Django_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import ContactForm
+
+def home(request):
+    return HttpResponse("I love Python!")
+
+def home(request):
+   return HttpResponse("<h1>I love Python!</h1>")
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contact(request):
+    return HttpResponse('contact view')
+
+
+def contact(request):
+    form = ContactForm()
+    return render(request, 'form.html', {'form': form})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+        email = form.cleaned_data['email']
+        print(name, email)
+    form = ContactForm()
+    return render(request, 'form.html', {'form': form})
